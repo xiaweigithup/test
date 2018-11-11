baed3677255cedf42e5c81ed0a73b04186a0c2ab
夏为 <2896916771@qq.com>
2018-11-11 20:38:41 +0800

    挖耳勺法规

--- src/夏为/夏为电话.java
+++ src/夏为/夏为电话.java
@@ -4,19 +4,6 @@ public class ÏÄÎªµç»° {
 
 	public static void main(String[] args) {
 		System.out.println("18271411805");
-		System.out.println("18271411805");
-		===================================
-		System.out.println("18271411805");
-		System.out.println("18271411805");
-		System.out.println("18271411805");
-		System.out.println("18271411805");
-		System.out.println("18271411805");
-		System.out.println("18271411805");
-		------------------------------------------
-		System.out.println("18271411805");
-		System.out.println("1==========05");
-		System.out.println("18---------05");
-		System.out.println("18271411805");
 		
 
 	}
