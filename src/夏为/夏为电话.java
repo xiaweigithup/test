92d2e55725fe5cee4c1ce6f9e6997531c60f5446
令狐冲 <18271411805@qq.com>
2018-11-11 20:46:17 +0800

    瓦子灯采访稿囧

--- src/夏为/夏为电话.java
+++ src/夏为/夏为电话.java
@@ -4,13 +4,13 @@ public class 夏为电话 {
 
 	public static void main(String[] args) {
 
-		为啥t7ihytcutiuioisxtf
+		/*为啥t7ihytcutiuioisxtf
 		染头发严格保护你家门口了，；h
 		rtyguhijokpfcghnjfubh
 		时代先锋，luikl
 		sddhgggfkhj
 		fdgfhmjkjlkhgjj
-		sdgfhgjhjfghjm
+		sdgfhgjhjfghjm*/
 		
 	}
 
