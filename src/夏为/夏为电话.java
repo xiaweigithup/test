ae2d5d758623ed266d491f5db744ab4837ea8057
夏为 <2896916771@qq.com>
2018-11-11 20:44:26 +0800

    二塔时以偶评论

--- src/夏为/夏为电话.java
+++ src/夏为/夏为电话.java
@@ -4,13 +4,14 @@ public class 夏为电话 {
 
 	public static void main(String[] args) {
 
-		System.out.println("18271411805");
-		System.out.println("1==========05");
-		System.out.println("18---------05");
-		System.out.println("18271411805");
-
-		System.out.println("18271411805");
-
+		为啥t7ihytcutiuioisxtf
+		染头发严格保护你家门口了，；h
+		rtyguhijokpfcghnjfubh
+		时代先锋，luikl
+		sddhgggfkhj
+		fdgfhmjkjlkhgjj
+		sdgfhgjhjfghjm
+		
 	}
 
 }
