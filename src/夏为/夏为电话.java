878e8260f88d610ed973a33c55a9cdf3153fd153
令狐冲 <18271411805@qq.com>
2018-11-11 20:40:51 +0800

    瓦尔特法规科

--- src/夏为/夏为电话.java
+++ src/夏为/夏为电话.java
@@ -4,21 +4,10 @@ public class ÏÄÎªµç»° {
 
 	public static void main(String[] args) {
 		System.out.println("18271411805");
-		System.out.println("18271411805");
-		===================================
-		System.out.println("18271411805");
-		System.out.println("18271411805");
-		System.out.println("18271411805");
-		System.out.println("18271411805");
-		System.out.println("18271411805");
-		System.out.println("18271411805");
-		------------------------------------------
-		System.out.println("18271411805");
 		System.out.println("1==========05");
 		System.out.println("18---------05");
 		System.out.println("18271411805");
 		
-
 	}
 
 }
